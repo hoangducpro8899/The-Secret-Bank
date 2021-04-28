b62e2120d84c50c602bf1a2bb86842889661056e
hoangducpro8899 <57797874+hoangducpro8899@users.noreply.github.com>
2021-04-28 10:01:09 +0700

    Update AccountAction.java

--- AccountAction.java
+++ AccountAction.java
@@ -2,4 +2,9 @@ public interface AccountAction {
     void goodBye();
     void show();
     void createAccount();
+    String login(String id);
+    void SaveBank(String fName);
+    void SaveUser(String fName);
+    void readFile(String fName1, String fName2);
+    void rut(String id);
 }
